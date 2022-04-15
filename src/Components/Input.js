222e8a4b324597fc1a3c792a13e027bd48481b42
kessie <kayp3477@gmail.com>
2022-04-15 21:58:07 +0000

    Added some functionalities

--- src/Components/Input.js
+++ src/Components/Input.js
@@ -1,7 +1,8 @@
 import React from "react";
+import "./Input.css";
 
-function Input() {
-  return <div>Input</div>;
+function Input(props) {
+  return <div className="Input">{props.input}</div>;
 }
 
 export default Input;

b91f902fb61f3c6b48818d1b57698eb6ad9c2d50
kessie <kayp3477@gmail.com>
2022-04-15 21:53:43 +0000

    Added some text below the calculator

---
+++ src/Components/FooterText.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./FooterText.css";
+
+function FooterText() {
+  return (
+    <div>
+      <h2 className="Text">Made with ❤ by Peekay_Codes with React.JS </h2>
+    </div>
+  );
+}
+
+export default FooterText;
